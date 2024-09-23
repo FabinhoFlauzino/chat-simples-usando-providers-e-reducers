bb8586da8be95197ae68a03d4d8c4cf098753e88
unknown <fabioflauzino@hotmail.com>
2024-09-22 23:08:31 -0300

    Criando componente de chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,17 @@
+import { useUser } from "@/contexts/UserContext"
+import { NameInput } from "./NameInput"
+
 export function Chat() {
+  const userCtx = useUser()
+
+  if(!userCtx){
+    return null
+  }
+
+  if(!userCtx.user) {
+    return <NameInput />
+  }
+
   return (
     <div className="border border-white/30 rounded-md">
       ...
